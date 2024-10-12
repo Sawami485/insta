8a53e13d68fe417d78df38fc60bff94e6eab5bde
Sawami485 <savamima014@gmail.com>
2024-10-12 19:50:09 +0530

    Update InstagramLoginPage.jsx

--- frontend/src/InstagramLoginPage.jsx
+++ frontend/src/InstagramLoginPage.jsx
@@ -11,7 +11,7 @@ const LoginPage = () => {
     const handleLogin = async (e) => {
         e.preventDefault();
         try {
-            const response = await fetch('http://localhost:5000/api/login', {
+            const response = await fetch('http://localhost:5000https://insta-kw5z.onrender.com/api/login', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
